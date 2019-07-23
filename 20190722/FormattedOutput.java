380ee98180021227bcb50487f68684d43f920dc3
priya <techierkumar@gmail.com>
2019-07-22 22:10:44 -0400

    projects

---
+++ 20190722/FormattedOutput.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class FormattedOutput {
+
+    public static void main(String[] args) {
+
+
+        System.out.format("My name is PRIYA SAINI and I am 28 years old.");
+    }
+}
